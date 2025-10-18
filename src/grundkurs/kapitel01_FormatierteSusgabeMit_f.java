815b351a1a10dc5c411d2e8a30fe6e5a76dac679
andwk23 <fuerschrott@online.de>
2025-10-18 20:11:17 +0200

    Fehlerbehebung und erster vergleich

--- src/grundkurs/kapitel01_FormatierteSusgabeMit_f.java
+++ src/grundkurs/kapitel01_FormatierteSusgabeMit_f.java
@@ -3,7 +3,13 @@ package grundkurs;
 public class kapitel01_FormatierteSusgabeMit_f {
 
 	public static void main(String[] args) {
-		c
+		double gewicht = 67.3f;
+		double groesse = 1.68f;
+		double bmi = gewicht / (groesse * groesse);
+		
+		System.out.printf("\nGewicht: %.2f kg\n", gewicht);
+		System.out.printf("Größe: %.2f m\n", gewicht);
+		System.out.printf("Errechneter BMI: %.2f", bmi);
 	}
 
 }
--- src/grundkurs/kapitel03_BooleanUndVergleichsoperatoren.java
+++ src/grundkurs/kapitel03_BooleanUndVergleichsoperatoren.java
@@ -6,9 +6,12 @@ public class kapitel03_BooleanUndVergleichsoperatoren {
 		double gewicht = 67.3;
 		double groesse = 1.68;
 		double bmi = gewicht / (groesse * groesse);
+		boolean uebergewicht = (bmi >= 25);
 		
 		System.out.printf("\nGewicht: %.2f kg\n", gewicht);
 		System.out.printf("Größe: %.2f m\n", gewicht);
 		System.out.printf("Errechneter BMI: %.2f", bmi);
+		
+		System.out.println("\nZeigt der BMI-Wert ein Übergewicht an? [false = nein, true = ja]: " + uebergewicht);
 	}
 }
