bda3dfc7e73d71c1e2103e905c0167ecb9d2b04f
andwk23 <fuerschrott@online.de>
2025-10-18 16:52:25 +0200

    Ergänzung

--- src/grundkurs/kapitel00.java
+++ src/grundkurs/kapitel00.java
@@ -5,6 +5,7 @@ public class kapitel00 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Nochmal 'Hallo Welt' :-(");
+		System.out.println("Nochmal 'Hallo Welt' :-(");
 
 	}
 
