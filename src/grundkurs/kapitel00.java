22edc4a87a59c6ccddf21399a595e3788a02225f
andwk23 <fuerschrott@online.de>
2025-10-18 16:47:34 +0200

    initial commit

---
+++ src/grundkurs/kapitel00.java
@@ -0,0 +1,11 @@
+package grundkurs;
+
+public class kapitel00 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Nochmal 'Hallo Welt' :-(");
+
+	}
+
+}
