68cddba4953a65a44d019ddda7d80922e2eb3f01
andwk23 <fuerschrott@online.de>
2025-10-18 20:28:13 +0200

    Einführung <, <=,>, >=

--- src/grundkurs/kapitel03_BooleanUndVergleichsoperatoren.java
+++ src/grundkurs/kapitel03_BooleanUndVergleichsoperatoren.java
@@ -7,11 +7,22 @@ public class kapitel03_BooleanUndVergleichsoperatoren {
 		double groesse = 1.68;
 		double bmi = gewicht / (groesse * groesse);
 		boolean uebergewicht = (bmi >= 25);
+		boolean untergewicht = (bmi < 18);
 		
 		System.out.printf("\nGewicht: %.2f kg\n", gewicht);
 		System.out.printf("Größe: %.2f m\n", gewicht);
-		System.out.printf("Errechneter BMI: %.2f", bmi);
+		System.out.printf("Errechneter BMI: %.2f\n", bmi);
 		
 		System.out.println("\nZeigt der BMI-Wert ein Übergewicht an? [false = nein, true = ja]: " + uebergewicht);
+		System.out.println("Zeigt der BMI-Wert ein Untergewicht an? [false = nein, true = ja]: " + untergewicht);
+		
+		if(uebergewicht) {
+			System.out.println("\nEine Diät ist angesagt!");
+		}
+		else if(untergewicht){
+			System.out.println("Das Gewicht ist zu gering!");
+		}else{
+			System.out.println("\nDas Gewicht liegt in Normbereich.");
+		}
 	}
 }
