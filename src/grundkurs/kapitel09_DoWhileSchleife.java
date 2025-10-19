925ae7283c3de88f02091926aebb90bca66c00c0
andwk23 <fuerschrott@online.de>
2025-10-19 15:23:22 +0200

    Do-While-Schleife wird mindestens einmal durchlaufen

---
+++ src/grundkurs/kapitel09_DoWhileSchleife.java
@@ -0,0 +1,39 @@
+package grundkurs;
+
+public class kapitel09_DoWhileSchleife {
+
+	public static void main(String[] args) {
+		double groesse = 1.68;
+		double gewicht = 42.0;
+		//Tabellenkopf
+
+		System.out.println("Gewicht\tGröße\tBMI-Wert   Auswertung");
+		System.out.println("---------------------------------------");
+		
+		do {
+			
+			double bmi = gewicht / (groesse * groesse);
+			
+			System.out.printf("%.2f\t%.2f\t%.2f\t   ", gewicht,groesse,bmi);
+			
+			int intBMI = (bmi>=25) ? 1 : (bmi < 18) ? -1 : 0;
+			
+			switch (intBMI) {
+			case 0:
+				System.out.println("Normalgewicht");
+				break;
+			case 1:
+				System.out.println("Übergewicht");
+				break;
+			case -1:
+				System.out.println("Untergewicht");
+				break;
+			default:
+				throw new IllegalArgumentException("Unexpected value: " + intBMI);
+			}
+			gewicht +=2;
+		}while (gewicht <= 80.0);
+
+	}
+
+}
